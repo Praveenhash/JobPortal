2dcf7da585aef3bad9a4e7a134fb06170e6e217a
Praveenhash <praveenkumarsulikeri@gmail.com>
2025-07-27 01:53:47 +0530

    committing all the files of Job app

---
+++ jobapplication/src/main/java/com/praveen/jobapplication/controller/JobController.java
@@ -0,0 +1,60 @@
+package com.praveen.jobapplication.controller;
+
+import com.praveen.jobapplication.Entity.Job;
+import com.praveen.jobapplication.service.JobService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class JobController {
+
+    private JobService jobService;
+
+    public JobController(JobService jobService){
+        this.jobService = jobService;
+    }
+
+    @GetMapping("/jobs")
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
+
+    }
+
+    @GetMapping("/jobs/{id}")
+    public ResponseEntity<Job> findJobById(@PathVariable Long id){
+        Job job = jobService.findJobById(id);
+        if(job != null){
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/jobs")
+    public ResponseEntity<String> createJob(@RequestBody Job job){
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job has been created successfully", HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("jobs/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job job){
+        boolean updated = jobService.updateJob(id, job);
+        if(updated)
+            return new ResponseEntity<>( "Job has been updated successfully;", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean delete = jobService.deleteJob(id);
+        if(delete){
+            return new ResponseEntity<>("Job deleted sucessfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ jobapplication/src/main/java/com/praveen/jobapplication/service/JobService.java
@@ -0,0 +1,20 @@
+package com.praveen.jobapplication.service;
+
+import com.praveen.jobapplication.Entity.Job;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface JobService {
+
+    List<Job> findAll();
+    void createJob(Job job);
+
+    Job findJobById(Long id);
+
+    boolean updateJob(Long id, Job job);
+
+
+    boolean deleteJob(Long id);
+}
